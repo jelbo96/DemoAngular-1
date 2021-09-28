ddd4653509840d51510a3cca417d862ef0bdf56b
Ignacio Jelvez <ijelvezh@gmail.com>
2021-09-28 17:06:14 -0300

    add cart validations

--- src/app/general/products/products.component.ts
+++ src/app/general/products/products.component.ts
@@ -8,7 +8,8 @@ import { ProductsService } from '../../services/products.service';
   styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
-  /* carrito: any = []; */
+  carrito: any = [];
+  montoTotal: number = 0;
 
   constructor(
     public categoriesService: CategoriesService,
@@ -17,9 +18,31 @@ export class ProductsComponent implements OnInit {
     console.log('print desde component', productsService.productos);
   }
 
-  /*   appendToCart(product) {
-    this.carrito.push(product);
-  } */
+  appendToCart(product: any) {
+    if (this.carrito.includes(product)) {
+      // si el producto ya esta en el carrito
+      if (
+        this.carrito[this.carrito.indexOf(product)].cantidad < product.stock
+      ) {
+        this.carrito[this.carrito.indexOf(product)].cantidad++;
+        this.montoTotal = this.montoTotal + product.price;
+      } else {
+        alert('No hay stock suficiente');
+      }
+    } else {
+      // si el producto no esta en el carrito
+      product['cantidad'] = 1;
+      this.carrito.push(product);
+      this.montoTotal = this.montoTotal + product.price;
+    }
+
+    console.log(this.carrito, this.montoTotal);
+  }
+
+  deleteItem(product: any) {
+    this.carrito.splice(this.carrito.indexOf(product), 1);
+    this.montoTotal = this.montoTotal - product.price * product.cantidad;
+  }
 
   showData() {
     /* Esta funcion se esta llamando desde el onInit */
